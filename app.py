1c56c6f5e146a1b53f92954c50118b1483c8d384
shmousaviy-blip <s.h.mousaviy@gmail.com>
2026-02-16 22:55:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -162,14 +162,13 @@ if data_file:
             except Exception as e:
                 st.error(f"Connection Error: {e}")
 else:
-    st.info("Waiting for Sales Data to be uploaded...")
+    st.info("Waiting for Sales Data to be uploaded from the sidebar...")
 
 # --- Header Section ---
-st.set_page_config(page_title="Sales AI Assistant", page_icon="📈", layout="wide")
+st.set_page_config(page_title="Data Analysis Assistant", page_icon="📈", layout="wide")
 
 st.markdown("""
-    <h1 style='text-align: center; color: #4A90E2;'>🚀 هوش مصنوعی تحلیل فروش</h1>
-    <p style='text-align: center; color: #888;'>فایل اکسل خود را آپلود کنید و تحلیل‌های پیشرفته بگیرید</p>
+    <h1 style='text-align: center; color: #4A90E2;'>🚀 Data Analysis Assistant</h1>
     <hr>
 """, unsafe_allow_html=True)
 
@@ -180,12 +179,13 @@ st.markdown("<br><br><br>", unsafe_allow_html=True)
 st.markdown("---")
 footer_html = f"""
 <div style="text-align: center;">
-    <p style="margin-bottom: 10px;">👨‍💻 توسعه داده شده توسط <b>حسن موسوی</b></p>
+    <p style="margin-bottom: 10px;">👨‍💻 Developed By <b>Hassan Moosavi</b></p>
     <a href="https://wa.me/31685529172" target="_blank"><img src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" style="margin:5px;"></a>
     <a href="http://linkedin.com/in/hassan-moosavi" target="_blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" style="margin:5px;"></a>
     <a href="mailto:s.h.mousaviy@gmail.com"><img src="https://img.shields.io/badge/Email-D14836?style=for-the-badge&logo=gmail&logoColor=white" style="margin:5px;"></a>
 </div>
 """
 st.markdown(footer_html, unsafe_allow_html=True)
+
 
 
